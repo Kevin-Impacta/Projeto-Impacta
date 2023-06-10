95c8a37ec84b9a11376eaa6646c71eba350e6747
Kevin-Impacta <132235972+Kevin-Impacta@users.noreply.github.com>
2023-06-10 17:54:05 -0300

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+import React from 'react';
+
+export default props => (
+  <footer className="footer">
+    <span>
+      Desenvolvido por <strong> Kev<span className="text-primary">i</span>n</strong>
+    </span>
+  </footer>
+);
